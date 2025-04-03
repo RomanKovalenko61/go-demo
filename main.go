c2443f3868d257a9c1eb7d95f0b79298bf11b641
Roman <astek14@mail.ru>
2025-04-03 20:32:13 +0300

    3.6 another variant

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ import (
 )
 
 func main() {
-	var userHeight = 1.8
-	var userKg float64 = 100
-	var IMT = userKg / math.Pow(userHeight, 2)
-
+	userHeight, userKg := 1.8, 100.0
+	IMT := userKg / math.Pow(userHeight, 2)
 	fmt.Print(IMT)
 }
