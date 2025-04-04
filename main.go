9fdc1d1f0595d3bbe6765b3946bdb9a97159bd10
Roman <astek14@mail.ru>
2025-04-04 18:39:27 +0300

    4.7. Exercise - Multiple Return values

--- main.go
+++ main.go
@@ -6,16 +6,8 @@ import (
 )
 
 func main() {
-	var userHeight float64
-	var userKg float64
-
 	fmt.Println("___ Калькулятор индекса массы тела ___")
-	fmt.Print("Введите свой рост в сантиметрах: ")
-	fmt.Scan(&userHeight)
-
-	fmt.Print("Введите свой вес: ")
-	fmt.Scan(&userKg)
-
+	userKg, userHeight := getUserInput()
 	IMT := calculateIMT(userKg, userHeight)
 	outputResult(IMT)
 }
@@ -29,4 +21,14 @@ func calculateIMT(userKg float64, userHeight float64) float64 {
 	const IMTPower = 2
 	IMT := userKg / math.Pow(userHeight / 100, IMTPower)
 	return IMT
+}
+
+func getUserInput() (float64, float64) {
+	var userHeight float64
+	var userKg float64
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userKg)
+	return userKg, userHeight
 }
