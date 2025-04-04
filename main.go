9408206d65e9995d8f56a28df67621062dfb804d
Roman <astek14@mail.ru>
2025-04-04 17:34:42 +0300

    4.1 input output

--- main.go
+++ main.go
@@ -7,7 +7,17 @@ import (
 
 func main() {
 	const IMTPower = 2
-	userHeight, userKg := 1.8, 100.0
+	var userHeight float64
+	var userKg float64
+
+	fmt.Print("___ Калькулятор индекса массы тела ___ \n")
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeight)
+
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userKg)
+	fmt.Print("Ваш индекс массы тела: ")
+
 	IMT := userKg / math.Pow(userHeight, IMTPower)
 	fmt.Print(IMT)
 }
