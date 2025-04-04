5a0d1b98c29292bca112eb2374f2eeb41fe09fce
Roman <astek14@mail.ru>
2025-04-04 17:44:52 +0300

    4.2 printf

--- main.go
+++ main.go
@@ -10,14 +10,13 @@ func main() {
 	var userHeight float64
 	var userKg float64
 
-	fmt.Print("___ Калькулятор индекса массы тела ___ \n")
-	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Println("___ Калькулятор индекса массы тела ___")
+	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userKg)
-	fmt.Print("Ваш индекс массы тела: ")
 
-	IMT := userKg / math.Pow(userHeight, IMTPower)
-	fmt.Print(IMT)
+	IMT := userKg / math.Pow(userHeight / 100, IMTPower)
+	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
 }
